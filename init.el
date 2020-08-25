f12e102eff4cd0262650952d0d0dd6bb71af4ac9
shyam <shyamsundar.shanmugam@wipro.com>
2020-08-25 20:30:47 +0530

    adding support for mermaid js

--- init.el
+++ init.el
@@ -14,8 +14,8 @@
 (setq vc-follow-symlinks t)
 
 (require 'org)
-(add-to-list 'org-structure-template-alist
-             '("x" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC"))
+;(add-to-list 'org-structure-template-alist
+;             '("x" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC"))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/shyam-emacs.org"))
 (custom-set-variables
@@ -28,28 +28,24 @@
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
-   (quote
-    ("816bacf37139d6204b761fea0d25f7f2f43b94affa14aa4598bce46157c160c2" "bf390ecb203806cbe351b966a88fc3036f3ff68cd2547db6ee3676e87327b311" default)))
+   '("816bacf37139d6204b761fea0d25f7f2f43b94affa14aa4598bce46157c160c2" "bf390ecb203806cbe351b966a88fc3036f3ff68cd2547db6ee3676e87327b311" default))
  '(fci-rule-color "#383838")
  '(fzf/executable "/usr/local/bin/fzf")
  '(graphviz-dot-dot-program "/usr/local/bin/dot")
  '(graphviz-dot-indent-width 2)
  '(markdown-command "~/go/bin/mdtool +h +ta +l")
  '(nrepl-message-colors
-   (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(org-agenda-files
-   (quote
-    ("~/projects/inovalon/inovalon-notes.org" "~/shyam/home/payments.org" "~/shyam/macbook/today.org" "~/shyam/macbook/buf.org")))
+   '("~/projects/inovalon/inovalon-notes.org" "~/shyam/home/payments.org" "~/shyam/macbook/today.org" "~/shyam/macbook/buf.org"))
  '(package-selected-packages
-   (quote
-    (fzf zenburn-theme typescript-mode powershell dired-subtree projectile edit-indirect php-mode plantuml-mode restclient nord-theme htmlize wsd-mode cql-mode kotlin-mode csv-mode graphviz-dot-mode yasnippet yaml-mode which-key web-mode vlf use-package try terraform-mode markdown-mode json-mode groovy-mode go-mode emmet-mode dockerfile-mode cypher-mode command-log-mode base16-theme auto-complete)))
- '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
- '(plantuml-default-exec-mode (quote jar))
+   '(mermaid-mode fzf zenburn-theme typescript-mode powershell dired-subtree projectile edit-indirect php-mode plantuml-mode restclient nord-theme htmlize wsd-mode cql-mode kotlin-mode csv-mode graphviz-dot-mode yasnippet yaml-mode which-key web-mode vlf use-package try terraform-mode markdown-mode json-mode groovy-mode go-mode emmet-mode dockerfile-mode cypher-mode command-log-mode base16-theme auto-complete))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
+ '(plantuml-default-exec-mode 'jar)
+ '(plantuml-jar-path "/usr/local/Cellar/plantuml/1.2020.15/libexec/plantuml.jar")
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
+   '((20 . "#BC8383")
      (40 . "#CC9393")
      (60 . "#DFAF8F")
      (80 . "#D0BF8F")
@@ -66,7 +62,7 @@
      (300 . "#7CB8BB")
      (320 . "#8CD0D3")
      (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
+     (360 . "#DC8CC3")))
  '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
