e63af866b8fb35af724ff3842c3fdbf60831fec5
shyam <xshyamx@gmail.com>
2022-08-25 19:37:26 +0530

    using fira code

--- init.el
+++ init.el
@@ -71,5 +71,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-(put 'set-goal-column 'disabled nil)
+ '(fixed-pitch ((t (:family "Fira Code")))))
+
