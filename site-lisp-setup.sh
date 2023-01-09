a45c2ca33843256ae04f86fd2e66620dafc62e22
shyam <shyamsundar.shanmugam@wipro.com>
2023-01-09 10:12:40 +0530

    move site-lisp setup out of site-lisp so, that site-lisp can be ignored

---
+++ site-lisp-setup.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+clone() {
+		src="$1"
+		target="$2"
+		if [ "$target" == "" ]; then
+				target=$(basename $src .git)
+		fi
+		if [ -e "./$target" ]; then
+				printf "\e[32m%s\e[0m already exists\n" $target
+		else
+				git clone $src $target
+		fi
+}
+download() {
+		src="$1"
+		target="$2"
+		if [ "$target" == "" ]; then
+				echo specify target file
+				return 1
+		fi
+		dir=$(dirname $target)
+		if [ "$dir" != "" ]; then
+				mkdir -p $dir
+		fi
+		if [ -e "./$target" ]; then
+				printf "\e[32m%s\e[0m already exists\n" $target
+		else
+				curl -o "./$target" -L "$src"
+		fi
+}
+mkdir -p site-lisp
+cd site-lisp
+download https://raw.githubusercontent.com/emacsmirror/emacswiki.org/master/visual-basic-mode.el visual-basic-mode/visual-basic-mode.el
+
+clone https://github.com/pdorrell/rules-editing-mode.git drools-mode
+clone https://github.com/xshyamx/fzf.el.git fzf
+clone https://github.com/pashky/restclient.el restclient
+clone https://github.com/ccod/dbd-mode dbdiagram-mode
+if ! grep "(provide" dbdiagram-mode/dbdiagram-mode.el > /dev/null; then
+    echo "(provide 'dbdiagram-mode)" >> dbdiagram-mode/dbdiagram-mode.el
+fi
+
+clone https://github.com/emacsattic/gedcom.git
--- site-lisp/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p visual-basic-mode
-curl -o visual-basic-mode/visual-basic-mode.el -L https://raw.githubusercontent.com/emacsmirror/emacswiki.org/master/visual-basic-mode.el
-git clone https://github.com/pdorrell/rules-editing-mode.git drools-mode
-git clone https://github.com/xshyamx/fzf.el.git fzf
-git clone https://github.com/pashky/restclient.el restclient
-git clone https://github.com/ccod/dbd-mode dbdiagram-mode
