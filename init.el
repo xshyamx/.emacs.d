aeb4b46460c95e27721b1923a3aa6b56ef7e3e00
shyam <shyamsundar.shanmugam@wipro.com>
2023-01-09 16:15:36 +0530

    move customizations to separate file

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 ;;; Initialize package
 (require 'package)
+;; Add melpa repository
 (add-to-list
  'package-archives
  '("melpa" . "http://melpa.org/packages/")
@@ -14,20 +15,11 @@
 
 ;;; have this as early as possible
 (setq vc-follow-symlinks t)
+
+;; Move customizations to separate file
+;; mainly `package-selected-packages' & `fixed-pitch' font
+(setq custom-file (locate-user-emacs-file "customizations.el"))
+(load-file custom-file)
+
 ;;; load literate configuration
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-	 '(visual-basic-mode ox-reveal lsp-metals company company-mode rust-mode svelte-mode rjsx-mode scala-mode org org-drill adoc-mode mermaid-mode fzf zenburn-theme typescript-mode powershell dired-subtree projectile edit-indirect php-mode plantuml-mode nord-theme htmlize wsd-mode cql-mode kotlin-mode csv-mode graphviz-dot-mode yasnippet yaml-mode which-key web-mode vlf use-package try terraform-mode markdown-mode json-mode groovy-mode go-mode emmet-mode dockerfile-mode cypher-mode command-log-mode auto-complete)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Fira Code")))))
-
