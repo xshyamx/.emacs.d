0bea8fba0b665c09f078ba59dbd028cfd52b1650
shyam <shyamsundar.shanmugam@wipro.com>
2023-02-02 08:47:58 +0530

    move work holidays to its separate file

---
+++ work-holidays.el
@@ -0,0 +1,46 @@
+(defconst work-holidays-2023
+	'(((3 22 2023) "Gudi Padva / Ugadi")
+		((6 29 2023) "Id-ul-Zuha/ Bakrid")
+		((9 19 2023) "Ganesh Chaturthi")
+		((10 24 2023) "Dussehra / Vijaya Dashmi"))
+	"List of holidays in 2023")
+
+(defconst work-holidays-2022
+	'(((1 14 2022) "Pongal")
+		((4 14 2022) "Dr. Ambedkar Jayanti")
+		((4 15 2022) "Good Friday")
+		((5 3 2022) "Ramzan/Id-ul-Fitr")
+		((8 31 2022) "Ganesh Chaturthi")
+		((10 5 2022) "Dussehra / Vijaya Dashami")
+		((10 24 2022) "Diwali"))
+	"List of holidays in 2022")
+
+(defconst work-holidays-2020
+	'(((1 15 2020) "Pongal")
+		((3 10 2020) "Holi")
+		((3 25 2020) "Ugadi")
+		((5 25 2020) "Ramzan/Id-ul-Fitr")
+		((8 22 2020) "Ganesh Chaturthi")
+		((10 26 2020) "Dussehra / Vijaya Dashami")
+		((11 14 2020) "Diwali"))
+	"List of holidays in 2020")
+
+(defconst work-holidays-2019
+	'(((6 5 2019) "Ramzan (Id-ul-Fitr)")
+		((9 2 2019) "Ganesh Chaturthi")
+		((10 8 2019) "Dussehra / Vijaya Dashami")
+		((10 28 2019) "Diwali"))
+	"List of holidays in 2019")
+
+(defconst work-holidays-2018
+	'(((6 15 2018) "Ramzan (Id-ul-Fitr)")
+		((10 19 2018) "Dussehra / Vijaya Dashami")
+		((11 6 2018) "Diwali"))
+	"List of holidays in 2018")
+
+(defconst work-holidays-2017
+	'(((3 29 2017) "Ugadi / Gudi Padwa")
+		((6 26 2017) "Ramzan (Id-ul-Fitr)")
+		((8 25 2017) "Ganesh Chaturthi")
+		((10 19 2017) "Diwali (Laxmi Puja)"))
+	"List of holidays in 2017")
