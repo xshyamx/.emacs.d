13b79cfa58e6d55eed5c16669909f4eccac7bf26
shyam <shyamsundar.shanmugam@wipro.com>
2024-05-03 11:29:11 +0530

    Add temp-buffer & require files from lisp folder

---
+++ lisp/temp-buffer.el
@@ -0,0 +1,14 @@
+;;; temp-buffer.el --  -*- lexical-binding: t -*-
+
+;; Open temp buffer
+(defun temp-buffer ()
+	"Switch to `*temp*' buffer.
+If buffer does not exist, create it first."
+	(interactive)
+	(switch-to-buffer
+	 (get-buffer-create "*temp*")))
+
+(keymap-global-set "C-x t" #'temp-buffer)
+
+(provide 'temp-buffer)
+;;; temp-buffer.el -- Ends here
