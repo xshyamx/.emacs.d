ef43561c8e5cf039ee1545484b92c8681566a8a6
shyam <shyamsundar.shanmugam@wipro.com>
2024-07-18 07:33:19 +0530

    Save & load project notes

--- lisp/project-notes.el
+++ lisp/project-notes.el
@@ -12,6 +12,30 @@
 (defvar project-meeetings-headline "Meetings"
 	"Headline for the meetings top-level heading")
 
+(defun project-info (file)
+	(let ((fn (expand-file-name file projects-home))
+				(m))
+		(push (file-name-base file) m)
+		(push :project m)
+		(push file m)
+		(push :file m)
+		(when (string-match (rx (or bow "/") (group (repeat 4 digit)) "--" (+ (not "/")) "/") file)
+			(push (string-to-number (match-string 1 file)) m)
+			(push :year m))
+		;; insert first 1k of the file and extract title & year elements
+		(with-temp-buffer
+			(insert-file-contents fn nil 0 1024)
+			(goto-char 0)
+			(when (re-search-forward (rx bol "#+title:" (+ space) (group (+ any)) eol) nil t)
+				(push (match-string-no-properties 1) m)
+				(push :title m))
+			(goto-char 0)
+			(unless (plist-member m :year)
+				(when (re-search-forward (rx bol "#+year:" (+ space) (group (+ any)) eol) nil t)
+					(push (string-to-number (match-string-no-properties 1)) m)
+					(push :year m))))
+		m))
+
 (defun add-project-meeting-note ()
 	"Find heading to add today's meetings"
 	(let ((ts (format-time-string "[%Y-%m-%d %a]"))
@@ -39,11 +63,15 @@
 
 (defun add-project-meeting-note-capture (key name file)
 	"Add a capture template for project meetings in the project notes"
-	(add-to-list 'org-capture-templates
-							 `(,key ,(format "%s: Meeting Notes" name) plain
-								 (file+function ,file add-project-meeting-note)
-								 "*** %?
-**** Meeting Details                                                 :noexport:" :jump-to-captured t)))
+	(let ((template (concat
+									 "*** %?\n"
+									 "**** Meeting Details" (format "%56s" ":noexport:")
+									 "\n")))
+		(add-to-list 'org-capture-templates
+								 `(,key ,(format "%s: Meeting Notes" name) plain
+												(file+function ,file add-project-meeting-note)
+												,template
+												:jump-to-captured t))))
 
 (defun setup-meeting-note-capture (key search-suffix project-shortname)
 	(add-hook
@@ -59,5 +87,52 @@
 				 ,project-shortname
 				 (expand-file-name (cdr current-project) projects-home))))))
 
+(defun add-project-weekly-update-capture (key name file template)
+	"Create a project specific capture template for weekly updates"
+	(add-to-list 'org-capture-templates
+							 `(,key ,(format "%s: Weekly Update" name) entry
+											(file+headline ,file "Weekly Updates")
+											,template :jump-to-captured t)))
+
+(defun setup-project-weekly-update-note-capture
+		(key search-suffix project-shortname template)
+	"Hook to `project-notes-hook' and add the capture weekly template"
+	(add-hook
+	 'project-notes-hook
+	 `(lambda ()
+			(when-let
+					(current-project
+					 (seq-find
+						(lambda (p) (string-suffix-p ,search-suffix (car p)))
+						project-notes nil))
+				(add-project-weekly-update-capture
+				 ,key
+				 ,project-shortname
+				 (expand-file-name (cdr current-project) projects-home)
+				 ,template)))))
+
+(defun write-project-notes ()
+	"Write `project-notes' value to file"
+	(with-temp-buffer
+		(insert ";;; -*- coding: utf-8; mode: lisp-data -*-\n")
+		(insert ";;; Automatically generated at: " (format-time-string "%FT%T%z") "\n")
+		(pp project-notes (current-buffer))
+		(write-region (point-min) (point-max) (locate-user-emacs-file "project-notes"))))
+
+(defun read-project-notes ()
+	"Read value of `project-notes' from file"
+	(with-temp-buffer
+		(insert-file-contents (locate-user-emacs-file "project-notes"))
+		(read (current-buffer))))
+
+(defun project-notes-updated-since (days)
+	"Check comment line for upated timestamp was more than number of
+DAYS specified"
+	(with-temp-buffer
+		(insert-file-contents (locate-user-emacs-file "project-notes") nil 0 256)
+		(goto-char 0)
+		(forward-line)
+		(when (re-search-forward "generated at: \\(.*\\)$")
+			(> (time-to-number-of-days (time-since (match-string-no-properties 1))) days))))
 (provide 'project-notes)
 ;;; project-notes.el -- Ends here
