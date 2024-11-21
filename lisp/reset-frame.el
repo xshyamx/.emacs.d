15d56bf06593d927f80b2599f2357ffcbee681e2
shyam <shyamsundar.shanmugam@wipro.com>
2024-11-21 09:38:13 +0530

    Add command to reset frame to defaults

---
+++ lisp/reset-frame.el
@@ -0,0 +1,26 @@
+;;; reset-frame.el --- Reset frame to preferred size  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  S. Shyam Sundar
+
+;; Author: S. Shyam Sundar <shyamsundar.shanmugam@wipro.com>
+;; Keywords: frames
+
+;;; Commentary:
+
+;;; Reset current frame to defaults
+
+;;; Code:
+
+(defvar default-frame-width 100
+	"Default frame width for `reset-frame'")
+(defvar default-frame-height 40
+	"Default frame height for `reset-frame'")
+
+(defun reset-frame (&optional frame)
+	"Resize specified frame to the defaults"
+	(interactive)
+	(set-frame-width (or frame (selected-frame)) default-frame-width)
+	(set-frame-height (or frame (selected-frame)) default-frame-height))
+
+(provide 'reset-frame)
+;;; reset-frame.el ends here
