2f6c9ea5bcb91f68f030286e777105845788ae21
shyam <xshyamx@gmail.com>
2025-01-08 10:57:18 +0530

    Skeletons for init-local & post-init-local files

---
+++ lisp/post-init-local.el
@@ -0,0 +1,17 @@
+;;; post-init-local.el --- post-initializations        -*- lexical-binding: t; -*-
+
+;; Author: shyam
+;; Keywords: convenience
+
+;;; Commentary:
+
+;; Initialization file to set variables with values relevant in the
+;; context of this host/config. Applied after literate config
+
+;; Use for late/deferred initialization
+
+;;; Code:
+
+
+(provide 'post-init-local)
+;;; post-init-local.el ends here
