f6408b2881ad98426087fbe304a3228f19979130
shyam <shyamsundar.shanmugam@wipro.com>
2025-02-04 09:05:11 +0530

    Add prefix-find-file based on prefix-directories

---
+++ lisp/prefix-find-file.el
@@ -0,0 +1,45 @@
+;;; prefix-find-file.el --- Prefix find file            -*- lexical-binding: t; -*-
+
+;; Author: shyam
+;;; Commentary: convenience
+
+;; Override find-file keybinding to look at specified directories
+;; based on the number of universal arguments
+
+;;; Code:
+
+(defcustom prefix-directories nil
+	"List of directories to search for files based on the number of
+universal arguments passed to the `prefix-find-file' command"
+	:type '(choice
+					(repeat :tag "List of directories" directory)
+					(directory :tag "Search directory\n" :value "~/")))
+
+(defun prefix--lookup-alist ()
+	"Construct prefix lookup list of the form (prefix . directory)
+from `prefix-directories'"
+	(let ((ds))
+		(seq-do-indexed (lambda (d i) (push (cons (expt 2 (+ 2 (* 2 i))) d) ds))
+										prefix-directories)
+		ds))
+
+(defun prefix-find-file (prefix)
+	"Replacement for `find-file' keybinding which can switch to
+opening files from a predefined list of directories from
+`prefix-directories' using the universal argument.
+Eg.
+C-x C-f          Opens the find file in default directory
+
+C-u C-x C-f      Opens the find file in the first directory from `prefix-directories' etc.
+C-u C-u C-x C-f  Opens the find file in the second directory from `prefix-directories' etc."
+	(interactive "p")
+	(let ((dir (assoc-default prefix (prefix--lookup-alist))))
+		(if dir
+				(let ((default-directory dir))
+					(call-interactively #'find-file))
+			(call-interactively #'find-file))))
+
+(keymap-global-set "C-x C-f" #'prefix-find-file)
+
+(provide 'prefix-find-file)
+;;; prefix-find-file.el
