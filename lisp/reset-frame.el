a50f721c9e7cf51c91b6f5c3554bc1b2edeffd35
shyam <shyamsundar.shanmugam@wipro.com>
2025-03-06 21:00:52 +0530

    Add frame position to reset

--- lisp/reset-frame.el
+++ lisp/reset-frame.el
@@ -15,12 +15,32 @@
   "Default frame width for `reset-frame'")
 (defvar default-frame-height 40
   "Default frame height for `reset-frame'")
+(defvar default-frame-position '(231 . 717)
+	"Default frame position (`top' . `left'")
 
-(defun reset-frame (&optional frame)
+(defun maximize-frame (&optional frame)
+	"Maximize the FRAME"
+	(interactive)
+	(set-frame-parameter
+	 (or frame (selected-frame))
+	 'fullscreen 'maximized))
+
+(defun reset-frame (prefix &optional frame)
   "Resize specified frame to the defaults"
-  (interactive)
+  (interactive "p")
   (set-frame-width (or frame (selected-frame)) default-frame-width)
-  (set-frame-height (or frame (selected-frame)) default-frame-height))
+  (set-frame-height (or frame (selected-frame)) default-frame-height)
+	(unless (>= prefix 4)
+		(set-frame-parameter
+		 (or frame (selected-frame))
+		 'fullscreen
+		 nil)
+		(set-frame-parameter
+		 (or frame (selected-frame))
+		 'top (car default-frame-position))
+		(set-frame-parameter
+		 (or frame (selected-frame))
+		 'left (cdr default-frame-position))))
 
 (provide 'reset-frame)
 ;;; reset-frame.el ends here
