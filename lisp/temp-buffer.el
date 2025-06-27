5a31680e373ab69b36a62c4b0a75b24eb27e1bc3
shyam <shyamsundar.shanmugam@wipro.com>
2025-06-27 21:29:53 +0530

    Launch temporary buffer in specific mode

--- lisp/temp-buffer.el
+++ lisp/temp-buffer.el
@@ -1,12 +1,36 @@
 ;;; temp-buffer.el --  -*- lexical-binding: t -*-
+;;;
+
+;;; Commentary:
+
+;;; Create temporary buffers
+
+;;; Code:
+
+
+
+(defvar temp-buffer-mode-list
+  '(json-mode xml-mode emacs-lisp-mode restclient-mode)
+  "List of modes to start temporary buffers")
 
 ;; Open temp buffer
-(defun temp-buffer ()
-  "Switch to `*temp*' buffer.
+(defun temp-buffer (prefix)
+  "Switch to `*temp*' buffer. Use with prefix `C-u' to switch to a temporary buffer with the selected mode. `temp-buffer-mode-list' contains the list of modes which can be selected.
+
 If buffer does not exist, create it first."
-  (interactive)
+  (interactive "p")
   (switch-to-buffer
-   (get-buffer-create "*temp*")))
+   (if (> prefix 1)
+       (when-let ((mode (completing-read
+			 "Select mode: "
+			 temp-buffer-mode-list nil t)))
+	 (let ((buffer (get-buffer-create
+			(format "*%s*"
+				(string-remove-suffix "-mode" mode)))))
+	   (with-current-buffer buffer
+	     (funcall (intern mode)))
+	   buffer))
+     (get-buffer-create "*temp*"))))
 
 (keymap-global-set "C-x t" #'temp-buffer)
 
